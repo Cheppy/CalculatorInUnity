3ee7d1e5cfd3c80c371362c6f6af952f62558201
Nick Letunovski <teamasterln@gmail.com>
2018-10-20 19:18:16 +0300

    sum

--- Assets/OperationsController.cs
+++ Assets/OperationsController.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class OperationsController : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		
+	void Summator(string number) {
+		float.Parse(number,
+      System.Globalization.CultureInfo.InvariantCulture);
 	}
 	
 	// Update is called once per frame
