cd37e0cbae8ece7d39c0ffbf4cb172cc4442eaca
Nick Letunovski <teamasterln@gmail.com>
2018-10-21 00:51:55 +0300

    :pencil: Plus finished;

--- Assets/OperationsController.cs
+++ Assets/OperationsController.cs
@@ -6,15 +6,21 @@ using UnityEngine.UI;
 public class OperationsController : MonoBehaviour {
 //[SerializeField] private InputField calcScreen;
 private string operationName;
-private float prevValue;
+private float prevValue=0.0f;
+private bool ifOpPressed=false;
 	// Use this for initialization
 	public void Summator(InputField calcScreen) {
-		if (calcScreen.text!=""){
-		prevValue =float.Parse(calcScreen.text,
+		//ifOpPressed=false;
+		if  (calcScreen.text!=""){
+			prevValue =prevValue+float.Parse(calcScreen.text,
       								System.Globalization.CultureInfo.InvariantCulture);
-		operationName = "+";
+			operationName = "+";
+			ifOpPressed = true;
 		}
-	}
+		else 
+			prevValue=0;
+		calcScreen.text="";
+		Debug.Log(prevValue);	}
 	
 	// Update is called once per frame
 	void Update () {
