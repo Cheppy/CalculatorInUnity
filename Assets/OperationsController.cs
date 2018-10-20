0370dc2a1d5c7152dbf5610936ea25d831d28b74
Nick Letunovski <teamasterln@gmail.com>
2018-10-21 00:13:42 +0300

    :pencil: added plus saving
    Added Sum saver;

--- Assets/OperationsController.cs
+++ Assets/OperationsController.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OperationsController : MonoBehaviour {
-
+//[SerializeField] private InputField calcScreen;
+private string operationName;
+private float prevValue;
 	// Use this for initialization
-	public void Summator(string number) {
-		float.Parse(number,
-      System.Globalization.CultureInfo.InvariantCulture);
+	public void Summator(InputField calcScreen) {
+		if (calcScreen.text!=""){
+		prevValue =float.Parse(calcScreen.text,
+      								System.Globalization.CultureInfo.InvariantCulture);
+		operationName = "+";
+		}
 	}
 	
 	// Update is called once per frame
