005eee0c680ead6e9ff368b63d5cbee2e612cf55
Nick Letunovski <teamasterln@gmail.com>
2018-10-22 22:16:11 +0300

    :pencil: Fixed plus, added other operations

--- Assets/OperationsController.cs
+++ Assets/OperationsController.cs
@@ -9,18 +9,71 @@ private string operationName;
 private float prevValue=0.0f;
 private bool ifOpPressed=false;
 	// Use this for initialization
+	public void RE(InputField calcScreen){
+		calcScreen.text="";
+		prevValue=0.0f;
+	}
+
 	public void Summator(InputField calcScreen) {
 		//ifOpPressed=false;
 		if  (calcScreen.text!=""){
 			prevValue =prevValue+float.Parse(calcScreen.text,
       								System.Globalization.CultureInfo.InvariantCulture);
 			operationName = "+";
-			ifOpPressed = true;
-		}
+		
+			}
 		else 
 			prevValue=0;
 		calcScreen.text="";
-		Debug.Log(prevValue);	}
+		
+		Debug.Log(prevValue);	
+	}
+		public void Minussator(InputField calcScreen) {
+		//ifOpPressed=false;
+		if  (calcScreen.text!=""){
+			prevValue =prevValue-float.Parse(calcScreen.text,
+      								System.Globalization.CultureInfo.InvariantCulture);
+			operationName = "-";
+		
+			}
+		else 
+			prevValue=0;
+		calcScreen.text="";
+		
+		Debug.Log(prevValue);	
+	}
+	public void Multiplator(InputField calcScreen) {
+		//ifOpPressed=false;
+		if  (calcScreen.text!=""){
+			prevValue =prevValue-float.Parse(calcScreen.text,
+      								System.Globalization.CultureInfo.InvariantCulture);
+			operationName = "*";
+		
+			}
+		else 
+			prevValue=0;
+		calcScreen.text="";
+		
+		Debug.Log(prevValue);	
+	}
+	public void Divator(InputField calcScreen) {
+		//ifOpPressed=false;
+		if  (calcScreen.text!=""){
+			if (prevValue!=0){
+			prevValue =prevValue/float.Parse(calcScreen.text,
+      								System.Globalization.CultureInfo.InvariantCulture);
+			operationName = "/";
+			}
+			else calcScreen.text="ERRROR %%00 PRESS RE";
+		
+			}
+		else 
+			prevValue=0;
+		calcScreen.text="";
+		
+		Debug.Log(prevValue);	
+	}
+	
 	
 	// Update is called once per frame
 	void Update () {
