429441b6186e9b7e0a21c36e00af532f6ee4920e
Nick Letunovski <teamasterln@gmail.com>
2018-10-24 19:20:50 +0300

    :pencil: added switch case

--- Assets/OperationsController.cs
+++ Assets/OperationsController.cs
@@ -4,39 +4,53 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class OperationsController : MonoBehaviour {
+
 	[SerializeField] private InputField calcScreen;
 	private string operationName;
 	private float prevValue;
 	private float secondOp;
 	private float memory;
-	private bool ifOpPressed = false;
-	//private InputField calcScreen;
-	//this section is all about monkeycode. TODO: make this part pretty like princess kenny
-	private bool ifMinusPressed = false;
-	//string operation;
-	// Use this for initialization
-	public void RE () {
-		//calcScreen.text = "";
-		prevValue = 0.0f;
-		ifMinusPressed = false;
-	}
-	public void Operations(int operation)
+	
+	public void RE () 
 	{
-		switch(operation)
-		{
-			case 1:  Minus(); break;
-		 
-		}
+		calcScreen.text = "";
+		prevValue = 0.0f;
 	}
-	public void Minus () { //Debug.Log(prevValue);
-		float onScreenValue = float.Parse (calcScreen.text,
+ 
+	public void OperationsCaller(int operation)
+	{
+			 switch(operation)
+			 {
+			 	case 1: Minus(); break;
+			// 	case Plusle:  Plusle(); break;
+			// 	case Dividor: Dividor(); break;
+			
+			}
+	}
+	
+	public void Plusle()
+	{
+
+	}
+	
+	public void Dividor()
+	{
+
+	}
+
+	public void Minus () 
+	{ //Debug.Log(prevValue);
+		 float onScreenValue = float.Parse (calcScreen.text,
 										   System.Globalization.CultureInfo.InvariantCulture
 										  );
 		prevValue = onScreenValue;
-		if (memory == 0) {
+		if (memory == 0) 
+		{
 			memory = prevValue;
 			Debug.Log (memory);
-		} else {
+		} 
+		else 
+		{
 			memory = memory - prevValue;
 			Debug.Log (memory);
 
