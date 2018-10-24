9a6c6122e259752be0d1301f3de35e0bf71ed899
Nick Letunovski <teamasterln@gmail.com>
2018-10-24 14:51:56 +0300

    :pencil: reworked operations, minus added

--- Assets/OperationsController.cs
+++ Assets/OperationsController.cs
@@ -4,84 +4,45 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class OperationsController : MonoBehaviour {
-	//[SerializeField] private InputField calcScreen;
+	[SerializeField] private InputField calcScreen;
 	private string operationName;
 	private float prevValue;
+	private float secondOp;
+	private float memory;
 	private bool ifOpPressed = false;
+	//private InputField calcScreen;
 	//this section is all about monkeycode. TODO: make this part pretty like princess kenny
 	private bool ifMinusPressed = false;
+	//string operation;
 	// Use this for initialization
-	public void RE (InputField calcScreen) {
-		calcScreen.text = "";
+	public void RE () {
+		//calcScreen.text = "";
 		prevValue = 0.0f;
 		ifMinusPressed = false;
 	}
+	public void Operations(int operation)
+	{
+		switch(operation)
+		{
+			case 1:  Minus(); break;
+		 
+		}
+	}
+	public void Minus () { //Debug.Log(prevValue);
+		float onScreenValue = float.Parse (calcScreen.text,
+										   System.Globalization.CultureInfo.InvariantCulture
+										  );
+		prevValue = onScreenValue;
+		if (memory == 0) {
+			memory = prevValue;
+			Debug.Log (memory);
+		} else {
+			memory = memory - prevValue;
+			Debug.Log (memory);
 
-	public void Summator (InputField calcScreen) {
-		//ifOpPressed=false;
-		if (calcScreen.text != "") {
-			prevValue = prevValue + float.Parse (calcScreen.text,
-												System.Globalization.CultureInfo.InvariantCulture
-												);
-			operationName = "+";
-
-		} 
-		else
-			prevValue = 0;
+		}
 		calcScreen.text = "";
-		Debug.Log (prevValue);
-	}
-	public void Minussator (InputField calcScreen) {
-		//ifOpPressed=false;
-
-		if (calcScreen.text != "") {
-			prevValue = prevValue - float.Parse (calcScreen.text,
-												 System.Globalization.CultureInfo.InvariantCulture
-												);
-			operationName = "-";
-
-		} 
-		else
-			prevValue = 0.0f;
-		calcScreen.text = "";
-		Debug.Log (prevValue);
 	}
 
-	public void Multiplator (InputField calcScreen) {
-		//ifOpPressed=false;
-		if (calcScreen.text != "") {
-			prevValue = prevValue * float.Parse (calcScreen.text,
-												 System.Globalization.CultureInfo.InvariantCulture
-												);
-			operationName = "*";
 
-		} 
-		else
-			prevValue = 0;
-		calcScreen.text = "";
-
-		Debug.Log (prevValue);
-	}
-	public void Divator (InputField calcScreen) {
-		//ifOpPressed=false;
-		if (calcScreen.text != "") {
-			if (prevValue != 0) {
-				prevValue = prevValue / float.Parse (calcScreen.text,
-					System.Globalization.CultureInfo.InvariantCulture);
-				operationName = "/";
-			} 
-			else calcScreen.text = "ERRROR %%00 PRESS RE";
-
-		} 
-		else
-			prevValue = 0;
-		calcScreen.text = "";
-
-		Debug.Log (prevValue);
-	}
-
-	// Update is called once per frame
-	void Start () {
-		Debug.Log (ifMinusPressed);
-	}
 }
